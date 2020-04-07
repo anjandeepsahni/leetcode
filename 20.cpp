a8764584652f4f08cb27980b249ad52af2487693
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2020-04-07 15:33:54 -0700

    Solution for LeetCode 20.

---
+++ 20.cpp
@@ -0,0 +1,73 @@
+/*
+
+20. Valid Parentheses
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+
+Example 2:
+
+Input: "()[]{}"
+Output: true
+
+Example 3:
+
+Input: "(]"
+Output: false
+
+Example 4:
+
+Input: "([)]"
+Output: false
+
+Example 5:
+
+Input: "{[]}"
+Output: true
+
+Solution:
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+*/
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        vector<char> stack;
+        for (auto &ch : s)
+        {
+            if (ch == '(' || ch == '{' || ch == '[')
+                stack.push_back(ch);
+            else
+            {
+                if ((stack.size() > 0) &&
+                    ((ch == ')' && stack.back() == ch - 1) ||
+                     (stack.back() == ch - 2)))
+                    stack.pop_back();
+                else
+                    return false;
+            }
+        }
+        return (stack.size() == 0);
+    }
+};
